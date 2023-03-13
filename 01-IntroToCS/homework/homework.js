97682a1152531743e8f2fb66def7fd73a45c613e
Kobi96 <tobiasolveira@outlook.com>
2023-03-13 15:40:05 -0300

    updates

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,44 +1,44 @@
-'use strict';
+"use strict";
 
 function BinarioADecimal(num) {
-   let result = 0;
-   let potencia = 0;
-   let array = [];
+  let result = 0;
+  let potencia = 0;
+  let array = [];
 
-   array = num.split("").reverse();
+  array = num.split("").reverse();
 
-   /*
+  /*
    "1000" 
-   ["1", "0", "0","0"] 
+   ["1", "0", "0","0"] g
    ["0", "0", "0","1"]
    */
 
-   for (let i = 0; i < array.length; i++) {
-      if (array[i] != 0) {
-         potencia = Math.pow(2, i);
-         result = result + potencia;
-      }
-   }
-   return result;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] != 0) {
+      potencia = Math.pow(2, i);
+      result = result + potencia;
+    }
+  }
+  return result;
 }
 
 function DecimalABinario(num) {
-   let binario = [];
+  let binario = [];
 
-   while (num !== 0) {
-      let div = num / 2;
-      binario.unshift(num % 2);
-      num = Math.trunc(div);
-   }
+  while (num !== 0) {
+    let div = num / 2;
+    binario.unshift(num % 2);
+    num = Math.trunc(div);
+  }
 
-   if (binario.length == 0) {
-      return "0"
-   } else {
-      return binario.join('');
-   }
-} 
+  if (binario.length == 0) {
+    return "0";
+  } else {
+    return binario.join("");
+  }
+}
 
 module.exports = {
-   BinarioADecimal,
-   DecimalABinario,
+  BinarioADecimal,
+  DecimalABinario,
 };
