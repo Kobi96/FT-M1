f59c33fca0dc41a0424c6cdca099ed01fddc19a1
Kobi96 <tobiasolveira@outlook.com>
2023-03-14 22:25:27 -0300

    updates 14-3-23

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,8 +1,36 @@
 "use strict";
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+  /*   let sum = 0;
 
-function DecimalABinario(num) {}
+  for (let i = 0; i < num.length; i++) {
+    sum += num[i] * 2 ** (num.length - 1 - i);
+  }
+  return sum; */
+
+  let suma = 0;
+
+  let number = num.split("").reverse();
+
+  for (let i = 0; i < number.length; i++) {
+    suma += number[i] * Math.pow(2, i);
+  }
+
+  return suma;
+}
+
+function DecimalABinario(num) {
+  let array = [];
+
+  while (num > 0) {
+    array.unshift(num % 2);
+    num = Math.floor(num / 2);
+  }
+
+  let result = array.join("");
+
+  return result;
+}
 
 module.exports = {
   BinarioADecimal,
