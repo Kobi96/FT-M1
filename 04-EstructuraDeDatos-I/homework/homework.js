5dcbdbbf1c807a9a7c240eaec12779a89ba66434
Kobi96 <tobiasolveira@outlook.com>
2023-03-28 23:29:19 -0300

    updates 28-3-23

--- 04-EstructuraDeDatos-I/homework/homework.js
+++ 04-EstructuraDeDatos-I/homework/homework.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /*
 Definir las funciones recursivas nFactorial y nFibonacci.
@@ -14,10 +14,23 @@ Secuencia:  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
 Como ejercicio adicional y completamente opcional, al terminar de resolver este problema pueden intentar definir funciones que logren los mismos resultados pero de manera iterativa.
 */
 
-function nFactorial(n) {}
+function nFactorial(n) {
+  if (n === 0) {
+    return 1;
+  }
 
-function nFibonacci(n) {}
+  return n * nFactorial(n - 1);
+}
 
+function nFibonacci(n) {
+  if (n === 0) {
+    return 0;
+  } else if (n === 1) {
+    return 1;
+  }
+
+  return nFibonacci(n - 2) + nFibonacci(n - 1);
+}
 /*
 Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO, donde el primer elemento que ingresa es el primero que se quita. Definir los siguientes métodos:
   - enqueue: agrega un valor respetando el orden.
@@ -31,7 +44,7 @@ function Queue() {}
 
 /*⚠️ No modificar nada debajo de esta línea ⚠️*/
 module.exports = {
-   Queue,
-   nFactorial,
-   nFibonacci,
+  Queue,
+  nFactorial,
+  nFibonacci,
 };
