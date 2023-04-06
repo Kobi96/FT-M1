1852b685b4e7d770aefd44bc62b2f143d747b7eb
Kobi96 <tobiasolveira@outlook.com>
2023-04-06 16:31:58 -0300

    updates 6-4-23

--- 06-EstructuraDeDatos-III/homework/homework.js
+++ 06-EstructuraDeDatos-III/homework/homework.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /*
  Implementar la clase BinarySearchTree, definiendo los siguientes métodos recursivos:
@@ -9,11 +9,90 @@
   - breadthFirstForEach: recorre el árbol siguiendo el orden breadth first (BFS)
   El ábrol utilizado para hacer los tests se encuentra representado en la imagen bst.png dentro del directorio homework.
 */
-function BinarySearchTree() {}
+function BinarySearchTree(value) {
+  (this.value = value), (this.left = null), (this.right = null);
+}
+
+BinarySearchTree.prototype.insert = function (value) {
+  if (value < this.value) {
+    if (!this.left) {
+      this.left = new BinarySearchTree(value);
+      return value;
+    } else {
+      this.left.insert(value);
+    }
+  } else {
+    if (!this.right) {
+      this.right = new BinarySearchTree(value);
+      return value;
+    } else {
+      this.right.insert(value);
+    }
+  }
+};
+
+BinarySearchTree.prototype.size = function () {
+  let count = 1;
+  if (this.left) count += this.left.size();
+  if (this.right) count += this.right.size();
+  return count;
+};
+
+BinarySearchTree.prototype.contains = function (value) {
+  if (value === this.value) return true;
+
+  if (this.left && this.left.contains(value)) return true;
+  if (this.right && this.right.contains(value)) return true;
+
+  return false;
+
+  /*  if (value < this.value) {
+    if (!this.left) return false;
+    else return this.left.contains(value);
+  } else {
+    if (!this.right) return false;
+    else return this.right.contains(value);
+  } */
+};
+
+BinarySearchTree.prototype.depthFirstForEach = function (cb, order) {
+  switch (order) {
+    case "post-order":
+      this.left && this.left.depthFirstForEach(cb, order);
+      this.right && this.right.depthFirstForEach(cb, order);
+      cb(this.value);
+      break;
+    case "pre-order":
+      cb(this.value);
+      this.left && this.left.depthFirstForEach(cb, order);
+      this.right && this.right.depthFirstForEach(cb, order);
+      break;
+    default:
+      this.left && this.left.depthFirstForEach(cb, order);
+      cb(this.value);
+      this.right && this.right.depthFirstForEach(cb, order);
+      break;
+  }
+};
+
+BinarySearchTree.prototype.breadthFirstForEach = function (cb, pendiente) {
+  if (!pendiente) {
+    var pendiente = [];
+  }
+
+  cb(this.value);
+
+  if (this.left) pendiente.push(this.left);
+  if (this.right) pendiente.push(this.right);
+
+  if (pendiente.length) {
+    pendiente.shift().breadthFirstForEach(cb, pendiente);
+  }
+};
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
 
 module.exports = {
-   BinarySearchTree,
+  BinarySearchTree,
 };
